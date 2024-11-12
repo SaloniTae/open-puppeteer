2ef290bd350b5d7404572d4c5a03c93c5278e1dc
naeemudheen p <nayeemudheenaslam3@gmail.com>
2024-11-12 23:27:53 +0530

    fixed vercel build issues

--- src/app/api/pptr/route.js
+++ src/app/api/pptr/route.js
@@ -1,20 +1,42 @@
+// app/api/check-page/route.js
 
-
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer'
+import puppeteerCore from 'puppeteer-core'
+import chromium from '@sparticuz/chromium'
+export const dynamic = 'force-dynamic'
 
 async function checkPageStatus(url) {
-  const browser = await puppeteer.launch();
+  let browser;
   let statusCode;
 
   try {
+    if (process.env.VERCEL_ENV === 'production') {
+      const executablePath = await chromium.executablePath()
+      browser = await puppeteerCore.launch({
+        executablePath,
+        args: chromium.args,
+        headless: chromium.headless,
+        defaultViewport: chromium.defaultViewport
+      })
+    } else {
+      browser = await puppeteer.launch({
+        headless: 'new',
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      })
+    }
+
     const page = await browser.newPage();
+
+    // Navigate to the specified URL and wait until network requests are idle
     const response = await page.goto(url, { waitUntil: 'networkidle2' });
+
+    // Check if the page loaded successfully
     statusCode = response && response.status() === 200 ? 200 : 404;
   } catch (error) {
     console.error('Error accessing page:', error);
     statusCode = 404;
   } finally {
-    await browser.close();
+    if (browser) await browser.close();
   }
 
   return statusCode === 200;
@@ -35,10 +57,10 @@ export async function GET(request) {
   console.log(status, "reals");
 
   return new Response(JSON.stringify({
-    statusCode: status,
+    statusCode: status ? 200 : 404,
     is200: status
-
   }), {
+    status: status ? 200 : 404,
     headers: { 'Content-Type': 'application/json' },
   });
 }
