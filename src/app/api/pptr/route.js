f9aec8d44d45f957c3b342cf1c20631d8014ed60
naeemudheen p <nayeemudheenaslam3@gmail.com>
2024-11-13 19:55:46 +0530

    Ready

--- src/app/api/pptr/route.js
+++ src/app/api/pptr/route.js
@@ -1,53 +1,68 @@
-
-
 import chromium from "@sparticuz/chromium-min";
-import puppeteer from 'puppeteer'
-import puppeteerCore from 'puppeteer-core'
-export const dynamic = 'force-dynamic'
+import puppeteerCore from "puppeteer-core";
+import puppeteer from "puppeteer";
+
+export const dynamic = "force-dynamic";
 
 const remoteExecutablePath =
   "https://github.com/Sparticuz/chromium/releases/download/v121.0.0/chromium-v121.0.0-pack.tar";
 
-async function checkPageStatus(url) {
-  let browser
-  let statusCode;
+let browser;
 
-  try {
+async function getBrowser() {
+  if (browser) return browser;
 
-    const browser = await puppeteerCore.launch({
+  if (process.env.NEXT_PUBLIC_VERCEL_ENVIRONMENT === "production") {
+    browser = await puppeteerCore.launch({
       args: chromium.args,
-      // See https://www.npmjs.com/package/@sparticuz/chromium#running-locally--headlessheadful-mode for local executable path 
       executablePath: await chromium.executablePath(remoteExecutablePath),
       headless: true,
     });
+  } else {
+    browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      headless: true,
+    });
+  }
+  return browser;
+}
 
+async function checkPageStatus(url) {
+  let statusCode;
+  try {
+    const browser = await getBrowser();
     const page = await browser.newPage();
-    const response = await page.goto(url, { waitUntil: 'networkidle2' });
+    const response = await page.goto(url, { waitUntil: "domcontentloaded" });
     statusCode = response && response.status() === 200 ? 200 : 404;
+    await page.close();
   } catch (error) {
-    console.error('Error accessing page:', error);
+    console.error("Error accessing page:", error);
     statusCode = 404;
-  } finally {
-    if (browser) await browser.close();
   }
   return statusCode === 200;
 }
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
-  const url = searchParams.get('url');
+  const url = searchParams.get("url");
   if (!url) {
-    return new Response(JSON.stringify({ error: 'URL parameter is required' }), {
-      status: 400,
-      headers: { 'Content-Type': 'application/json' },
-    });
+    return new Response(
+      JSON.stringify({ error: "URL parameter is required" }),
+      {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
   }
   const status = await checkPageStatus(url);
-  return new Response(JSON.stringify({
-    statusCode: status ? 200 : 404,
-    is200: status
-  }), {
-    status: status ? 200 : 404,
-    headers: { 'Content-Type': 'application/json' },
-  });
+  return new Response(
+    JSON.stringify({
+      statusCode: status ? 200 : 404,
+      is200: status,
+    }),
+    {
+      status: status ? 200 : 404,
+      headers: { "Content-Type": "application/json" },
+    }
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,52 +1,64 @@
-'use client'
+"use client";
 
-import { Copy, LoaderCircle } from 'lucide-react'
-import { useState, useEffect } from 'react'
+import { Copy, Github, LoaderCircle } from "lucide-react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [url, setUrl] = useState('')
-  const [status, setStatus] = useState('')
-  const [isLoading, setIsLoading] = useState(false)
-  const [apiEndpoint, setapiEndpoint] = useState(false)
+  const [url, setUrl] = useState("");
+  const [status, setStatus] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [apiEndpoint, setapiEndpoint] = useState(false);
 
   useEffect(() => {
     setapiEndpoint(`${process.env.NEXT_PUBLIC_APP_DOMAIN}api/pptr?url=`);
   }, []);
   async function fetchPageStatus() {
     if (url === "") {
-      alert("Enter valid url")
-      return
+      alert("Enter valid url");
+      return;
     }
-    setStatus('')
-    setIsLoading(true)
+    setStatus("");
+    setIsLoading(true);
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_APP_DOMAIN}api/pptr?url=${url}`);
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_APP_DOMAIN}api/pptr?url=${url}`
+      );
       const data = await response.json();
       const { is200 } = data;
-      setStatus(is200 ? 200 : 404)
-      return
+      setStatus(is200 ? 200 : 404);
+      return;
     } catch (error) {
       console.error("Error fetching page status:", error);
       return null;
-    }
-    finally {
-      setIsLoading(false)
+    } finally {
+      setIsLoading(false);
     }
   }
   return (
     <div className="container mx-auto flex flex-col items-center justify-center min-h-screen px-4 py-8">
-
-      <h1 className="text-4xl font-bold text-center mb-2 text-gray-800">
-        openPuppeteer
+      <h1 className="text-4xl font-bold text-center mb-2 text-gray-800 flex gap-2 justify-center items-center">
+        openPuppeteer{" "}
+        <a href="https://github.com/naeemudheenp/open-puppeteer">
+          <Github />
+        </a>
       </h1>
       <p className="text-xl text-center mb-8 text-gray-600">
         Free Puppeteer as a Service
       </p>
 
       <div className="w-full max-w-2xl">
-        <div className=' mb-3'>
-          <div className='text-black  font-semibold text-2xl'>Get Started</div>
-          <div className='text-black/40 '>Enter a URL to check its status or use our API</div>
+        <div className=" mb-3">
+          <div className="text-black  font-semibold text-2xl">Get Started</div>
+          <div className="text-black/40 ">
+            Enter a URL to check its status or use our API | Goto{" "}
+            <a
+              className=" text-black underline"
+              href="https://github.com/naeemudheenp/open-puppeteer"
+            >
+              Github
+            </a>{" "}
+            for documentation
+          </div>
         </div>
         <div className="space-y-8">
           <div className="flex space-x-2 relative">
@@ -57,21 +69,26 @@ export default function Home() {
               onChange={(e) => setUrl(e.target.value)}
               className="flex-grow border rounded-md p-4 text-black"
             />
-            <div className={` text-black  absolute right-[134px] top-0 bottom-0 bg-gray-100 my-3 rounded p-2 flex justify-center items-center ${status === "" && 'hidden'}`} >
+            <div
+              className={` text-black  absolute right-[134px] top-0 bottom-0 bg-gray-100 my-3 rounded p-2 flex justify-center items-center ${status === "" && "hidden"
+                }`}
+            >
               {status}
             </div>
             <button
               disabled={isLoading}
               onClick={() => {
-                fetchPageStatus()
+                fetchPageStatus();
               }}
-              className="whitespace-nowrap bg-black/70 hover:bg-black transition-all p-3 rounded-md">
-              {
-                isLoading ? <div className=' animate-spin '>
-
-                  <LoaderCircle color='white' />
-                </div> : 'Check Status'
-              }
+              className="whitespace-nowrap bg-black/70 hover:bg-black transition-all p-3 rounded-md"
+            >
+              {isLoading ? (
+                <div className=" animate-spin ">
+                  <LoaderCircle color="white" />
+                </div>
+              ) : (
+                "Check Status"
+              )}
             </button>
           </div>
           <div className="bg-gray-100 p-4 rounded-lg flex items-center space-x-2 overflow-x-auto">
@@ -79,14 +96,21 @@ export default function Home() {
             <code className="text-sm flex-grow text-black">{apiEndpoint}</code>
             <Copy
               onClick={() => {
-                navigator.clipboard.writeText(apiEndpoint)
-                alert("Copied")
+                navigator.clipboard.writeText(apiEndpoint);
+                alert("Copied");
               }}
-              className=' cursor-pointer'
-              color='black' />
+              className=" cursor-pointer"
+              color="black"
+            />
+          </div>
+
+          <div className=" flex justify-center items-center">
+            <a href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fnaeemudheenp%2Fopen-puppeteer">
+              <img src="https://vercel.com/button" alt="Deploy with Vercel" />
+            </a>
           </div>
         </div>
       </div>
-    </div >
-  )
+    </div>
+  );
 }
