2f07f3dae7b9c17cfc46d8bf02c1e4a6eb420e1a
naeemudheen p <nayeemudheenaslam3@gmail.com>
2024-11-14 19:56:30 +0530

    fixed ga

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -26,15 +26,12 @@ export default function RootLayout({ children }) {
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA}`}
         ></script>
-
-        <Script
-          id="ga"
-        >
+        <Script>
           {`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', ${process.env.NEXT_PUBLIC_GA});`}
+  gtag('config', '${process.env.NEXT_PUBLIC_GA}');`}
         </Script>
       </head>
       <body
