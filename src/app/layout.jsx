ff6f4c4c10d314801b41839322e8041e7c714d33
naeemudheen p <nayeemudheenaslam3@gmail.com>
2024-11-14 19:59:13 +0530

    fixed build issues

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -25,8 +25,8 @@ export default function RootLayout({ children }) {
         <script
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA}`}
-        ></script>
-        <Script>
+        />
+        <Script id="ga">
           {`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
